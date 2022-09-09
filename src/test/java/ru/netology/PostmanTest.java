489a641de418620e1539f6972b6a1b8eaee869e4
taya sapurina <sapurina0t@gmail.com>
2022-09-09 21:05:15 +0500

    add test

---
+++ src/test/java/ru/netology/PostmanTest.java
@@ -0,0 +1,22 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void shouldReturnSomeData() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("some data")
+                .when()
+                .post("/post")
+                .then()
+                .body("data", equalTo("some"))
+        ;
+    }
+}
